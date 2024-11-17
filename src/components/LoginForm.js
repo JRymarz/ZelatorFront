001d8d251f46f3180c40c5b1ea68538a082bd586
JRymarz <jakubrymarz1@gmail.com>
2024-11-17 21:38:09 +0100

    Poprawiono (nie ukończono) logowanie użytkownika.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,34 +1,62 @@
 import React, {useState} from "react";
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
 
 
 function LoginForm() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
         try {
-            const response = await axios.post('http://localhost:9002/login', {
+            const response = await axios.post("http://localhost:9002/auth/login", {
                 email,
-                password
+                password,
             });
 
-            console.log('Odpowiedz z backendu: ', response.data);
+            if(response.status === 200) {
+                navigate('/');
+            }
         } catch (error) {
-            console.error('Błąd logowania: ', error);
+            setError("Invalid credentails");
         }
+
+        // e.preventDefault();
+        //
+        // const formData = new URLSearchParams();
+        // formData.append('username', email);
+        // formData.append('password', password);
+        //
+        // try {
+        //     const response = await axios.post('http://localhost:9002/login', formData, {
+        //         headers: {
+        //             'Content-Type': 'application/x-www-form-urlencoded',
+        //         },
+        //         withCredentials: true,
+        //     });
+        //
+        //     // console.log('Odpowiedz z backendu: ', response.data);
+        //     if(response.status === 200){
+        //         navigate('/')
+        //     }
+        // } catch (error) {
+        //     console.error('Błąd logowania: ', error);
+        // }
     };
 
     return(
+        <div>
         <form onSubmit={handleSubmit}>
             <h2>Logowanie</h2>
             <div>
-                <label htmlFor={email}>E-mail:</label>
+                <label htmlFor="email">E-mail:</label>
                 <input
                     type="email"
-                    id="email"
+                    // id="email"
                     value={email}
                     placeholder="email"
                     onChange={(e) => setEmail(e.target.value)}
@@ -40,7 +68,7 @@ function LoginForm() {
                 <label htmlFor="password">Hasło:</label>
                 <input
                     type="password"
-                    id="password"
+                    // id="password"
                     value={password}
                     placeholder="hasło"
                     onChange={(e) => setPassword(e.target.value)}
@@ -49,6 +77,8 @@ function LoginForm() {
             </div>
             <button type="submit">Zaloguj</button>
         </form>
+        {error} && <p>{error}</p>
+        </div>
     )
 
 }
